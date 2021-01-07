13741c0909a19474c3c5a8522d7229411c1e7174
Tim Carry <tim@pixelastic.com>
2021-01-07 22:33:25 +0100

    wip

--- src/_scripts/transforms.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  raw(item) {
-    const raw = JSON.stringify(item, null, 4);
-    console.info(raw);
-    return raw;
-  },
-};
--- src/script.js
+++ src/script.js
@@ -1,4 +1,28 @@
 const theme = require('norska/theme');
+const { refinementList } = require('norska/frontend/algolia/widgets');
+// const lazyloadAttributes = require('norska/frontend/lazyload/attributes');
+
 (async () => {
-  await theme.init();
+  await theme.init({
+    searchParameters: {
+      facetingAfterDistinct: true,
+    },
+    widgets: [
+      {
+        type: refinementList,
+        options: {
+          container: '#tags',
+          attribute: 'playlist.title',
+          sortBy: ['count:desc', 'name:asc'],
+        },
+      },
+    ],
+    transforms: {
+      // preview(item) {
+      //   const previewUrl = item.picture.preview;
+      //   const options = { width: 600, placeholder: { width: 200 } };
+      //   return lazyloadAttributes(previewUrl, options);
+      // },
+    },
+  });
 })();
